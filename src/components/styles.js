481f81ec5c68dc4e08d14a54364c504c86104cd5
Uday khatri <udayskhatri@gmail.com>
2022-11-21 12:54:47 -0500

    styling change for suggestion card
    - CP-30

--- src/components/styles.js
+++ src/components/styles.js
@@ -70,11 +70,12 @@ const styles = () => {
     },
     suggestionCardRow: {
       opacity: 0.9,
-      marginTop:15,
+      marginTop: 20,
       width: windowWidth - 40,
       backgroundColor: theme.colors.cardColor,
       borderRadius: 15,
-      padding: 10,
+      paddingHorizontal: 15,
+      paddingVertical: 10,
       marginHorizontal: 20,
       flexDirection: "row",
       justifyContent: "space-between",
@@ -130,11 +131,6 @@ const styles = () => {
     },
     textContainer: {
       flex: 1,
-      marginTop: 0,
-      marginBottom: 0,
-    },
-    likeIcon:{
-      marginTop: 5,
     },
     showAllContainer: {
       textAlign: "center",
